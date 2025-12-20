bdb1792ec766c7899572b01624ab0b26c562c0b7
Matthieu Jeanson <matthieu.jeanson@gmail.com>
2025-12-20 22:38:28 +0100

    Start of part 2
    The current implementation running a Djikstra
    algorithm is too slow

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -3,6 +3,8 @@ use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use regex::Regex;
+use std::cmp;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -54,10 +56,6 @@ fn part1_fewest_button_presses(
 ) -> Result<usize> {
     let mut djikstra = vec![usize::MAX; usize::pow(2, num_lights)];
     djikstra[0] = 0;
-    println!(
-        "num_lights {num_lights}, len of djikstra {}",
-        djikstra.len()
-    );
     let mut visited = vec![false; usize::pow(2, num_lights)];
 
     loop {
@@ -88,6 +86,136 @@ fn part1_fewest_button_presses(
     }
 }
 
+// This is part of solving part 2
+// Not my algorithm, I could not figure it out, got too deep into algorithms
+// to solve systems of diophantine equations and matrix manipulation
+// it was interesting, but not practical here (and too generic)
+// The algorithm was described here:
+// https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+fn part1_all_button_combinations(num_lights: u32, target: usize, buttons: &Vec<usize>) -> Vec<usize> {
+    let num_buttons = buttons.len();
+    let mut answer: Vec<usize> = Vec::new();
+
+    // we don't want to try pressing no buttons
+    // the bits of try_button will tell us which buttons we are trying to press
+    for try_button in 1..usize::pow(2, num_buttons as u32) {
+        let mut try_result: usize = 0;
+        for button_bit in 0..num_buttons {
+            if try_button & (1 << button_bit) != 0 {
+                // trying with that button pressed
+                try_result ^= buttons[button_bit];
+            }
+        }
+        if try_result == target {
+            answer.push(try_button);
+        }
+    }
+    answer
+}
+
+fn part2_process_line(machine: String) -> Result<usize> {
+    // Read the buttons
+    let re_buttons = Regex::new(r"\((\s*\d+(?:,\s*\d+)*\s*)\)\s*").unwrap();
+    let mut buttons: Vec<usize> = Vec::new();
+    for button_caps in re_buttons.captures_iter(&machine) {
+        let mut this_button = 0;
+        for id_str in button_caps.get(1).unwrap().as_str().split(",") {
+            let id_num = usize::from_str_radix(id_str, 10).unwrap();
+            this_button |= 1 << id_num;
+        }
+        buttons.push(this_button);
+    }
+    println!("Captured buttons {:?} for buttons", { &buttons });
+
+    // then read the joltages
+    let re_joltage = Regex::new(r"\{([^}]+)\}").unwrap();
+    let mut joltage: Vec<usize> = Vec::new();
+    let joltage_cap = re_joltage.captures(&machine).unwrap();
+    for jolt_str in joltage_cap.get(1).unwrap().as_str().split(",") {
+        let jolt_num = usize::from_str_radix(jolt_str, 10).unwrap();
+        joltage.push(jolt_num);
+    }
+    println!("Got joltage {:?}", joltage);
+
+    part2_fewest_button_presses(&joltage, &buttons)
+}
+
+fn distance_between_joltages(source: &Vec<usize>, dest: &Vec<usize>) -> usize {
+    assert_eq!(source.len(), dest.len());
+    let mut answer = 0;
+    for i in 0..source.len() {
+        answer = cmp::max(answer, dest[i] - source[i]);
+    }
+    answer
+}
+
+fn part2_fewest_button_presses(joltage: &Vec<usize>, buttons: &Vec<usize>) -> Result<usize> {
+    let mut djikstra: HashMap<Vec<usize>, usize> = HashMap::new();
+    let mut visited: HashMap<Vec<usize>, bool> = HashMap::new();
+    let num_lights = joltage.len();
+    let start = vec![0; joltage.len()];
+    djikstra.insert(start, 0);
+
+    loop {
+        // find the smallest distance spent so far + minimum possible distance to target
+        let mut min_path = usize::MAX;
+        let mut min_index = Vec::new();
+        let mut path_so_far = usize::MAX;
+        for (index, d) in &djikstra {
+            if *d < min_path && !visited.contains_key(index) {
+                min_index = (*index).clone();
+                min_path = *d;
+                path_so_far = *d;
+            }
+        }
+        print!(
+            "So far {} and made {}, size of visited {}, going from {:?}\r",
+            min_path,
+            path_so_far,
+            visited.len(),
+            &min_index
+        );
+
+        //println!("Starting from {:?}", min_index);
+
+        assert_ne!(path_so_far, usize::MAX);
+        assert!(!visited.contains_key(&min_index));
+        visited.insert(min_index.clone(), true);
+
+        // try all the buttons and see if there is any path to update
+        let new_distance = path_so_far + 1;
+
+        'buttons: for b in buttons {
+            let mut new_dest = min_index.clone();
+            let mut on_target = true;
+
+            for i in 0..num_lights {
+                if (b & (1 << i)) != 0 {
+                    new_dest[i] += 1;
+                    if new_dest[i] > joltage[i] {
+                        continue 'buttons;
+                    }
+                }
+                if new_dest[i] != joltage[i] {
+                    on_target = false;
+                }
+            }
+
+            if on_target {
+                println!("Got one machine with {new_distance}");
+                return Ok(new_distance);
+            }
+
+            if !djikstra.contains_key(&new_dest) {
+                //println!("Can get to {:?} in {new_distance}", new_dest);
+                djikstra.insert(new_dest, new_distance);
+            }
+        }
+
+        //println!("Visited is now {:?}", visited.keys());
+    }
+}
+
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -95,7 +223,6 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
         let machines: Vec<String> = reader.lines().flatten().collect();
         let mut answer = 0;
         for m in machines {
@@ -112,17 +239,22 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(33, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        let machines: Vec<String> = reader.lines().flatten().collect();
+        let mut answer = 0;
+        for m in machines {
+            answer += part2_process_line(m).unwrap();
+        }
+        Ok(answer)
+    }
+
+    assert_eq!(33, part2(BufReader::new(TEST.as_bytes()))?);
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
